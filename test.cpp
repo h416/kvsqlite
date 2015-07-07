7440d62122adda8e5a4f72851502cd9f8399c359
h416 <h416@users.noreply.github.com>
2015-07-07 20:35:23 +0900

    use progress_timer

--- test.cpp
+++ test.cpp
@@ -1,5 +1,7 @@
-#include <kvsqlite/kvsqlite.h>
 #include <cassert>
+
+#include <kvsqlite/kvsqlite.h>
+#include <kvsqlite/progress_timer.h>
 
 class Person
 {
@@ -19,8 +21,10 @@ private:
 };
 
 int main(int argc, const char * argv[]) {
+    
     kvsqlite::database db("test.sqlite3");
-
+    kvsqlite::progress_timer t;
+    
     db.set("hello", "world");
 
     std::string value;
@@ -28,9 +32,31 @@ int main(int argc, const char * argv[]) {
 
     assert(value == "world");
     
+    db.remove("hello");
+    bool result = db.exists("hello");
+    assert(!result);
+    
     Person person("Tim");
     db.set("person1", person);
     assert(db.exists("person1"));
     
+    {
+        kvsqlite::progress_timer t2;
+        kvsqlite::transaction transaction(db);
+        for(int i=0;i<10000;++i){
+            Person p("name");
+            std::string key = std::to_string(i);
+            db.set(key, p);
+        }
+        transaction.commit();
+    }
+    
+    {
+        kvsqlite::progress_timer t2;
+        Person p("");
+        result = db.get("1", p);
+        assert(result);
+        assert(p.name()=="name");
+    }
     return 0;
 }
