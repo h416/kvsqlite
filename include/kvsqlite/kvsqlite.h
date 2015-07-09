5858fbf7b14e0a57c07eaaf1f7f0ca8276bb35ef
h416 <h416@users.noreply.github.com>
2015-07-09 22:54:25 +0900

    updated api

--- include/kvsqlite/kvsqlite.h
+++ include/kvsqlite/kvsqlite.h
@@ -10,6 +10,7 @@
 #include <sstream>
 #include <string>
 #include <memory>
+#include <vector>
 
 #include <dlib/sqlite.h>
 
@@ -87,6 +88,10 @@ class database : public dlib::database
     dlib::database::open(file);
     init();
   }
+  bool has_key(const std::string& key, const std::string& collection = "")
+  {
+    return exists(key, collection);
+  }
   bool exists(const std::string& key, const std::string& collection = "")
   {
     return find(key, collection) > 0;
@@ -102,16 +107,18 @@ class database : public dlib::database
     dlib::int64 rowid = find(key, collection);
 
     if(rowid > 0){
-      update_statement_->bind(1, data);
-      update_statement_->bind(2, metadata);
-      update_statement_->bind(3, rowid);
-      update_statement_->exec();
+      dlib::statement* st = update_statement();
+      st->bind(1, data);
+      st->bind(2, metadata);
+      st->bind(3, rowid);
+      st->exec();
     }else{
-      insert_statement_->bind(1, collection);
-      insert_statement_->bind(2, key);
-      insert_statement_->bind(3, data);
-      insert_statement_->bind(4, metadata);
-      insert_statement_->exec();
+      dlib::statement* st = insert_statement();
+      st->bind(1, collection);
+      st->bind(2, key);
+      st->bind(3, data);
+      st->bind(4, metadata);
+      st->exec();
     }
   }
   template<class T>
@@ -126,15 +133,16 @@ class database : public dlib::database
   }
   bool get(const std::string& key, std::string& data, const std::string& collection = "", std::string* metadata = nullptr)
   {
-    get_statement_->bind(1, collection);
-    get_statement_->bind(2, key);
-    get_statement_->exec();
+    dlib::statement* st = data_statement();
+    st->bind(1, collection);
+    st->bind(2, key);
+    st->exec();
 
-    get_statement_->move_next();
-    if(1 < get_statement_->get_num_columns()){
-      get_statement_->get_column(0, data);
+    st->move_next();
+    if(1 < st->get_num_columns()){
+      st->get_column(0, data);
       if(metadata){
-        get_statement_->get_column(1, *metadata);
+        st->get_column(1, *metadata);
       }
       return true;
     }else{
@@ -145,81 +153,158 @@ class database : public dlib::database
       return false;
     }
   }
+  // Deletes the database row with the given key/collection.
+  void del(const std::string& key, const std::string& collection = "")
+  {
+    remove(key, collection);
+  }
   void remove(const std::string& key, const std::string& collection = "")
   {
-    remove_statement_->bind(1, collection);
-    remove_statement_->bind(2, key);
-    remove_statement_->exec();
+    dlib::statement* st = remove_statement();
+    st->bind(1, collection);
+    st->bind(2, key);
+    st->exec();
   }
-  void removeAllObjectsInCollection(const std::string& collection)
+  // Deletes every key/object pair from the given collection.
+  void remove_collection(const std::string& collection)
   {
-    dlib::statement st(*this, "DELETE FROM \"kvsqlite\" WHERE \"collection\" = ?;");
-    st.bind(1, collection);
-    st.exec();
+    dlib::statement* st = remove_collection_statement();
+    st->bind(1, collection);
+    st->exec();
   }
-  void removeAll()
+  // Removes every key/object pair in the entire database (from all collections).
+  void clear()
   {
-    exec("DELETE FROM \"kvsqlite\";");
+    dlib::statement* st = clear_statement();
+    st->exec();
   }
 
-  int64 numberOfCollections()
+  // Returns the total number of collections.
+  int64 collection_count()
   {
-    dlib::statement st(*this, "SELECT COUNT(DISTINCT collection) AS NumberOfRows FROM \"kvsqlite\";");
+    dlib::statement* st = collection_count_statement();
     return count(st);
   }
-  int64 numberOfKeysInCollection(const std::string& collection)
+  // Returns the total number of keys in the given collection.
+  int64 count(const std::string& collection)
   {
-    dlib::statement st(*this, "SELECT COUNT(*) AS NumberOfRows FROM \"kvsqlite\" WHERE \"collection\" = ?;");
-    st.bind(1, collection);
+    dlib::statement* st = count_statement();
+    st->bind(1, collection);
     return count(st);
   }
-  int64 numberOfKeysInAllCollections()
+  // Returns the total number of key/object pairs in the entire database (including all collections).
+  int64 size()
   {
-    dlib::statement st(*this, "SELECT COUNT(*) AS NumberOfRows FROM \"kvsqlite\";");
+    dlib::statement* st = size_statement();
     return count(st);
+  }
+
+  // Returns a list of all collection names.
+  std::vector<std::string> collections()
+  {
+    return get_vector(collections_statement());
+  }
+
+  // Returns a list of all keys in the given collection.
+  std::vector<std::string> keys(const std::string &collection = "")
+  {
+    dlib::statement* st = keys_statement();
+    st->bind(1, collection);
+    return get_vector(st);
   }
 
  private:
   std::unique_ptr<dlib::statement> find_statement_;
-  std::unique_ptr<dlib::statement> get_statement_;
+  std::unique_ptr<dlib::statement> data_statement_;
   std::unique_ptr<dlib::statement> update_statement_;
   std::unique_ptr<dlib::statement> insert_statement_;
   std::unique_ptr<dlib::statement> remove_statement_;
+  std::unique_ptr<dlib::statement> remove_collection_statement_;
+  std::unique_ptr<dlib::statement> clear_statement_;
+  std::unique_ptr<dlib::statement> collection_count_statement_;
+  std::unique_ptr<dlib::statement> count_statement_;
+  std::unique_ptr<dlib::statement> size_statement_;
+  std::unique_ptr<dlib::statement> collections_statement_;
+  std::unique_ptr<dlib::statement> keys_statement_;
 
   void init()
   {
       create_table();
-      init_statements();
   }
-  void init_statements()
+  dlib::statement* statement(std::unique_ptr<dlib::statement> &st, const std::string &sql)
   {
-      find_statement_.reset (new dlib::statement(*this, "SELECT \"rowid\" FROM \"kvsqlite\" WHERE \"collection\" = ? AND \"key\" = ?;") );
-
-      get_statement_.reset (new dlib::statement(*this, "SELECT \"data\", \"metadata\" FROM \"kvsqlite\" WHERE \"collection\" = ? AND \"key\" = ?;"));
-
-      update_statement_.reset (new dlib::statement(*this, "UPDATE \"kvsqlite\" SET \"data\" = ?, \"metadata\" = ? WHERE \"rowid\" = ?;"));
-
-      insert_statement_.reset (new dlib::statement(*this, "INSERT INTO \"kvsqlite\""
-                         " (\"collection\", \"key\", \"data\", \"metadata\") VALUES (?, ?, ?, ?);"));
-
-      remove_statement_.reset (new dlib::statement(*this, "DELETE FROM \"kvsqlite\" WHERE \"collection\" = ? AND \"key\" = ?;"));
+    if(!st){
+      st.reset(new dlib::statement(*this, sql));
+    }
+    return st.get();
   }
+  dlib::statement* find_statement()
+  {
+    return statement(find_statement_, "SELECT \"rowid\" FROM \"kvsqlite\" WHERE \"collection\" = ? AND \"key\" = ?;");
+  }
+  dlib::statement* data_statement()
+  {
+    return statement(data_statement_, "SELECT \"data\", \"metadata\" FROM \"kvsqlite\" WHERE \"collection\" = ? AND \"key\" = ?;");
+  }
+  dlib::statement* update_statement()
+  {
+    return statement(update_statement_, "UPDATE \"kvsqlite\" SET \"data\" = ?, \"metadata\" = ? WHERE \"rowid\" = ?;");
+  }
+  dlib::statement* insert_statement()
+  {
+    return statement(update_statement_, "INSERT INTO \"kvsqlite\""
+                         " (\"collection\", \"key\", \"data\", \"metadata\") VALUES (?, ?, ?, ?);");
+  }
+  dlib::statement* remove_statement()
+  {
+    return statement(remove_statement_, "DELETE FROM \"kvsqlite\" WHERE \"collection\" = ? AND \"key\" = ?;");
+  }
+  dlib::statement* remove_collection_statement()
+  {
+    return statement(remove_collection_statement_, "DELETE FROM \"kvsqlite\" WHERE \"collection\" = ?;");
+  }
+  dlib::statement* clear_statement()
+  {
+    return statement(clear_statement_, "DELETE FROM \"kvsqlite\";");
+  }
+  dlib::statement* collection_count_statement()
+  {
+    return statement(collection_count_statement_, "SELECT COUNT(DISTINCT collection) AS NumberOfRows FROM \"kvsqlite\";");
+  }
+  dlib::statement* count_statement()
+  {
+    return statement(count_statement_, "SELECT COUNT(*) AS NumberOfRows FROM \"kvsqlite\" WHERE \"collection\" = ?;");
+  }
+  dlib::statement* size_statement()
+  {
+    return statement(size_statement_, "SELECT COUNT(*) AS NumberOfRows FROM \"kvsqlite\";");
+  }
+  dlib::statement* collections_statement()
+  {
+    return statement(collections_statement_, "SELECT DISTINCT \"collection\" FROM \"kvsqlite\";");
+  }
+  dlib::statement* keys_statement()
+  {
+    return statement(keys_statement_, "SELECT \"key\" FROM \"kvsqlite\" WHERE \"collection\" = ?;");
+  }
+
   bool table_exists(const std::string& tablename)
   {
-    return dlib::query_int(*this, "select count(*) from sqlite_master where name = \""+tablename+"\"") == 1;
+    return dlib::query_int(*this, "SELECT COUNT(*) FROM SQLITE_MASTER WHERE NAME = \""+tablename+"\"") == 1;
   }
 
   dlib::int64 find(const std::string& key, const std::string& collection = "")
   {
-    find_statement_->bind(1, collection);
-    find_statement_->bind(2, key);
-    find_statement_->exec();
+    dlib::statement* st = find_statement();
+    st->bind(1, collection);
+    st->bind(2, key);
+    st->exec();
 
-    find_statement_->move_next();
+    st->move_next();
     dlib::int64 rowid = 0;
-    if(0 < find_statement_->get_num_columns())
+    if(0 < st->get_num_columns())
     {
-      rowid = find_statement_->get_column_as_int64(0);
+      rowid = st->get_column_as_int64(0);
     }
     return rowid;
   }
@@ -238,14 +323,26 @@ class database : public dlib::database
     }
   }
 
-  int64 count(dlib::statement &st)
+  int64 count(dlib::statement *st)
   {
-    st.exec();
-    st.move_next();
+    st->exec();
+    st->move_next();
     dlib::int64 result = 0;
-    if(0 < st.get_num_columns())
+    if(0 < st->get_num_columns())
     {
-      result = st.get_column_as_int64(0);
+      result = st->get_column_as_int64(0);
+    }
+    return result;
+  }
+  std::vector<std::string> get_vector(dlib::statement *st)
+  {
+    std::vector<std::string> result;
+    st->exec();
+    while(st->move_next())
+    {
+      std::string collection;
+      st->get_column(0, collection);
+      result.push_back(collection);
     }
     return result;
   }
