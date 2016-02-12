371329c18c0022869a469245a524f534cf197298
h416 <h416@users.noreply.github.com>
2016-02-12 23:37:17 +0900

    * updated dlib 18.18 * updated sqlite 3.10.2

--- test.sh
+++ test.sh
@@ -5,14 +5,14 @@ if [ ! -d cereal-1.1.2 ]; then
     tar xf cereal-1.1.2.tar.gz
 fi  
 
-if [ ! -d dlib-18.16 ]; then  
-    curl -o dlib-18.16.tar.gz -L https://github.com/davisking/dlib/archive/v18.16.tar.gz
-    tar xf dlib-18.16.tar.gz
+if [ ! -d dlib-18.18 ]; then  
+    curl -o dlib-18.18.tar.gz -L https://github.com/davisking/dlib/archive/v18.18.tar.gz
+    tar xf dlib-18.18.tar.gz
 fi
 
-if [ ! -d sqlite-amalgamation-3081100 ]; then  
-    curl -O https://sqlite.org/2015/sqlite-amalgamation-3081100.zip
-    unzip sqlite-amalgamation-3081100.zip
+if [ ! -d sqlite-amalgamation-3100200 ]; then  
+    curl -O https://sqlite.org/2016/sqlite-amalgamation-3100200.zip
+    unzip sqlite-amalgamation-3100200.zip
 fi
 
 CC=${CC:-clang}
@@ -25,8 +25,8 @@ echo "Environment: `uname -a`"
 echo "Compiler: `$CXX --version`"
 
 
-$CC -c -O2 -I sqlite-amalgamation-3081002 -DSQLITE_THREADSAFE=0 -DSQLITE_OMIT_LOAD_EXTENSION -o sqlite.o sqlite-amalgamation-3081100/sqlite3.c
-$CXX -c -O2 -std=c++11 -I include -I cereal-1.1.2/include -I dlib-18.16 -I sqlite-amalgamation-3081002 -o test.o test.cpp
+$CC -c -O2 -I sqlite-amalgamation-3100200 -DSQLITE_THREADSAFE=0 -DSQLITE_OMIT_LOAD_EXTENSION -o sqlite.o sqlite-amalgamation-3100200/sqlite3.c
+$CXX -c -O2 -std=c++11 -I include -I cereal-1.1.2/include -I dlib-18.18 -I sqlite-amalgamation-3100200 -o test.o test.cpp
 $CXX -o test test.o sqlite.o
 
 ./test
